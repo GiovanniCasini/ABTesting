b98ff2749291ee7112504b92821a2717c495af9f
Giovanni Casini <giovanni.casini1@stud.unifi.it>
2021-09-01 16:45:53 +0200

    Initial commit

---
+++ wp-content/plugins/plugin-ABtesting/plugin-ABtesting.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Plugin Name: ABtesting
+    Description: Plugin per Wordpress che consente di fare A/B testing randomizzando gli utenti su versioni diverse del sito
+    Author: Casini Tanganelli
+    Version: 1.0
+*/ 
+
