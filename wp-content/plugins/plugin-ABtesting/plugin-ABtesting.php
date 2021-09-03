225ef02e56c31ab7f66ecb354216f5077794a05f
Cosimo Tanganelli <cosimo.tanganelli@stud.unifi.it>
2021-09-03 02:15:56 +0200

    aggiunta roba all'header

--- wp-content/plugins/plugin-ABtesting/plugin-ABtesting.php
+++ wp-content/plugins/plugin-ABtesting/plugin-ABtesting.php
@@ -1,9 +1,15 @@
 <?php
-/*
-    Plugin Name: ABtesting
-    Description: Plugin per Wordpress che consente di fare A/B testing randomizzando gli utenti su versioni diverse del sito
-    Author: Casini Tanganelli
-    Version: 1.0
-*/ 
+
+/**
+ * Plugin Name:       ABtesting
+ * Plugin URI:        https://localhost/wp-admin/plugins.php
+ * Description:       Plugin per Wordpress che consente di fare A/B testing randomizzando gli utenti su versioni diverse del sito
+ * Version:           1.0
+ * Requires PHP:      7.2
+ * Author:            Giovanni Casini & Cosimo Tanganelli
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:       /languages
+ */ 
 
 ?>
