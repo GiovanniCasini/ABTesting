9d5c07a8108ac35d9d95d96dddbf6bed1e70dd86
Giovanni Casini <giovanni.casini1@stud.unifi.it>
2021-12-01 16:33:55 +0100

    add database and testing logic

--- wp-content/plugins/plugin-ABtesting/plugin-ABtesting.php
+++ wp-content/plugins/plugin-ABtesting/plugin-ABtesting.php
@@ -16,8 +16,11 @@ function demo_settings()
     add_settings_section("section", "Temi da alternare", null, "demo");
     add_settings_field("theme1", "Tema 1", "theme_1_select", "demo", "section");
     add_settings_field("theme2", "Tema 2", "theme_2_select", "demo", "section");  
+    add_settings_section("urlsection", "URL", null, "demo");
+    add_settings_field("goalurl", "URL Traguardo", "urlTraguardo", "demo", "urlsection");
     register_setting("section", "theme1");
     register_setting("section", "theme2");
+    register_setting("urlsection", "goalurl");
 }
 
 function theme_1_select()
@@ -54,6 +57,11 @@ function theme_2_select()
    <?php
 }
 
+function urlTraguardo(){
+    $html = "<input type='text' value='" . get_option('goalurl') . "' name='goalurl'></input>";
+    echo $html;
+}
+
 add_action("admin_init", "demo_settings");
 
 function demo_page()
@@ -65,6 +73,7 @@ function demo_page()
          <form method="post" action="options.php">
             <?php
                settings_fields("section");
+               settings_fields("urlsection");
  
                do_settings_sections("demo");
                  
@@ -81,4 +90,56 @@ function menu_item()
 }
  
 add_action("admin_menu", "menu_item");
+
+function cookie_manager() {
+    $themes_array = [get_option('theme1'), get_option('theme2')];
+    $rand_index = array_rand($themes_array);
+    $user_theme = $themes_array[$rand_index];
+
+    if(!isset($_COOKIE['userid'])) {
+        setcookie('userid', uniqid('', true), 0, '/');
+        setcookie('theme', $user_theme, 0, '/');
+        setcookie('time_started', time(), 0, '/');
+        setcookie('clicks', 0, 0, '/');
+        switch_theme($user_theme);
+    }
+    else{
+        if(!isset($_COOKIE["total_time"])){
+            setcookie('clicks', $_COOKIE['clicks']+1, 0, '/');
+        }
+    }
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')$url = "https://";   
+    else $url = "http://";
+    $url.= $_SERVER['HTTP_HOST'];
+    $url.= $_SERVER['REQUEST_URI'];    
+      
+    //echo $url;
+    if($url == get_option('goalurl') && $_COOKIE["datatransfered"] != "true"){
+        $total_time = time() - $_COOKIE['time_started'];
+        setcookie('total_time', $total_time, 0, '/');
+        $servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "abtesting";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO testingdata (clicks, totaltime, userid, theme)
+VALUES ('". $_COOKIE["clicks"] ."', '". $total_time ."', '".  $_COOKIE["userid"] ."', '" . $_COOKIE["theme"] . "')";
+
+if (!mysqli_query($conn, $sql)) {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+else{
+    setcookie("datatransfered", "true", 0, "/");
+}
+
+mysqli_close($conn);
+    }
+   }
+add_action('init', 'cookie_manager');
 
